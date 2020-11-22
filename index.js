243b9da56db9d3659a8245ae6876160ab81902e1
DarkSide Labs <8547740+DarkSideLabs@users.noreply.github.com>
2020-11-22 19:00:50 +0100

    primo deploy su heroku

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 process.env.NTBA_FIX_319 = 1;
 const TOKEN = process.env.TELEGRAM_TOKEN || '1328058614:AAETGD56cXKya-YtiPok4Ua09sMZVfMcFmU';
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(TOKEN, {polling: true});
 /* ------------------------------------------------------------- HEROKU */
 const options = {
   webHook: {
-    // Port to which you should bind is assigned to $PORT variable
-    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
     port: process.env.PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
 // Heroku routes from port :443 to $PORT
@@ -18,12 +12,12 @@ const options = {
 // to get this automatically
 // See: https://devcenter.heroku.com/articles/dyno-metadata
 const url = process.env.APP_URL || 'https://redelcastello-bot.herokuapp.com:443';
-//const bot = new TelegramBot(TOKEN, options);
+const bot = new TelegramBot(TOKEN, options);
 
 
 // This informs the Telegram servers of the new webhook.
 // Note: we do not need to pass in the cert, as it already provided
-//bot.setWebHook(`${url}/bot${TOKEN}`);
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
 // Just to ping!
